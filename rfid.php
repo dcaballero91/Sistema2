a44ba14bcc14d8a7b6adbc953f8c98899a16ce8d
root <you@example.com>
2018-10-11 00:09:22 +0100

    falta corregir if

--- rfid.php
+++ rfid.php
@@ -4,44 +4,44 @@ include_once('Conexion_Abrir.php');
 $conexion = 5;
 if($_SERVER['REQUEST_METHOD'] == "POST"){
     // Get data
+    //print_r($REQUEST);
+    //print_r($_SERVER);
+    //print_r($_POST);
     $name = isset($_POST['name']) ? mysql_real_escape_string($_POST['name']) : "";
     $code = isset($_POST['code']) ? mysql_real_escape_string($_POST['code']) : "";
-   
+       
 
     $sql = "SELECT transaccion.IdTransaccion, transaccion.Cantidad, tag.Cod_Tag, transaccion.Cantidad * planes.Costo AS total
- FROM transaccion INNER JOIN tag INNER JOIN vehiculo inner join planes inner join Cliente
-where transaccion.idTag = tag.idTag and vehiculo.ID = planes.ID and tag.idCliente = Cliente.idCliente and  Cod_Tag= '$code'";
+ FROM transaccion INNER JOIN tag INNER JOIN vehiculo inner join planes inner join cliente
+where transaccion.idTag = tag.idTag and vehiculo.ID = planes.ID and tag.idCliente = cliente.idCliente and  Cod_Tag= '$code'";
 //echo $conexion;
 $rs       = mysql_query($sql);
-
+echo $rows;
+        if($rs!=NULL){
         while($rows = mysql_fetch_assoc($rs)){
                 
        
                 $estado=$rows['total'];
-
                // update transaccion set cantidad=cantidad+1 where Cod_Tag= '$code';
-                
                 if($estado<=300000){
-                        $sql2 = "SELECT idTag FROM Tag WHERE Cod_Tag='$code';";
+                        $sql2 = "SELECT idTag FROM tag WHERE Cod_Tag='$code';";
                         $rs2       = mysql_query($sql2);
                          while($rows2 = mysql_fetch_assoc($rs2)){
                              $idTag=$rows2['idTag'];
                              $sql3 ="UPDATE transaccion set cantidad=cantidad+1 where idTag= '$idTag';";
                              $rs3       = mysql_query($sql3);
-
-                         }
-                       
-                        $json = array("status" => 1, "msg" => "SIN_ERROR");
-                        
-
-                }
-                else {
-                         $json = array("status" => 0, "msg" => "ERROR");
-                 
-                 } 
-
+                             $json = array("status" => 0, "msg" => "SIN_ERROR");
+                         
+                        } 
+                       }else{
+                        $json = array("status" => 1, "msg" => "ERROR");  
+                      }
+                
         }
-
+        $json = array("status" => 2, "msg" => "ERROR");
+      }else{
+            $json = array("status" => 2, "msg" => "ERROR"); 
+}
 }
 
 
